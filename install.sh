62633971291d1981455a857e3e8340684c9e97c7
Jason Griffin <jason@magnetic.io>
2020-07-28 16:08:13 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -eu
+VAMP_INSTALLER_VERSION=0.0.1-alpha-22
+VAMP_INSTALLER_IMAGE=${DEFAULT_VAMP_INSTALLER_IMAGE:=vampio/k8s-installer:$VAMP_INSTALLER_VERSION}
+VAMP_INSTALLER_BOOTSTRAP_YAML=${DEFAULT_VAMP_BOOTSTRAP_YAML:=https://raw.githubusercontent.com/magneticio/vamp-cloud-installer/master/bootstrap-policy.yaml}
+
+# Apply policy required to be able to create the resources.
+kubectl apply -f "$VAMP_INSTALLER_BOOTSTRAP_YAML"
+
+# Run nats-setup container containing the latest set of manifests.
+kubectl run vamp-installer --image-pull-policy=Always --serviceaccount=vamp-installer --image=$VAMP_INSTALLER_IMAGE --restart=Never
+
+# Wait for the setup container to start or bail.
+kubectl wait --for=condition=Ready pod/vamp-installer --timeout=30s
+
+# Pass the custom parameters to the nats-setup container image.
+kubectl exec vamp-installer -- vamp-installer.sh "$@"
+
+# Remove the required policy for setup purposes.
+kubectl delete -f "$VAMP_INSTALLER_BOOTSTRAP_YAML"
+
+# Remove the setup pod.
+kubectl delete pod vamp-installer --grace-period=0 --force
