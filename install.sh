115e3f64b282f8c7df23a8af86645890548bf70c
Jason Griffin <jason@magnetic.io>
2020-12-05 10:22:09 +0100

    Bumped installer to v1.1.2-alpha2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -eu
-VAMP_INSTALLER_VERSION=1.1.2-alpha1
+VAMP_INSTALLER_VERSION=1.1.2-alpha2
 VAMP_INSTALLER_IMAGE=${DEFAULT_VAMP_INSTALLER_IMAGE:=vampio/k8s-installer:$VAMP_INSTALLER_VERSION}
 VAMP_INSTALLER_BOOTSTRAP_YAML=${DEFAULT_VAMP_BOOTSTRAP_YAML:=https://raw.githubusercontent.com/magneticio/vamp-cloud-installer/master/bootstrap-policy.yaml}
 
